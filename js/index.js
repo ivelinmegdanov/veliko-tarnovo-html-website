aefce33b6aa3e6faae08f9c2cc686e9487086a6a
Ivelin Megdanov <iv4o.megdanov@gmail.com>
2024-06-12 17:25:30 +0300

    Veliko Tarnovo html website

---
+++ js/index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const swiper = new Swiper(".swiper", {
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    loop: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
